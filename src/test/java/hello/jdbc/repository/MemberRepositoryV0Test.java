f74d03f2a5d2ce4e6c4b80ca683a0d5603db5110
hannah <ghdcoalss33@gmail.com>
2022-06-04 14:51:19 +0900

    feat(jdbc): JDBC 개발 - select

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,11 +2,13 @@ package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -16,7 +18,13 @@ class MemberRepositoryV0Test {
 
     @Test
     void crud() throws SQLException {
+        // save
         Member member = new Member("memberV0", 10000);
         memberRepository.save(member);
+
+        // findById
+        Member findMember = memberRepository.findById(member.getMemberId());
+        log.info("findMember : {}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
