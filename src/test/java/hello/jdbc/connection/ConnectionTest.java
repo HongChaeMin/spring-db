a0f82c8d32d483605ffdcc24d65be5858b18d688
홍채민 <coals0329@cocen.com>
2022-06-09 09:22:45 +0900

    feat(connection-pool): DataSource ex 2 - connection pool

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -2,6 +2,7 @@ package hello.jdbc.connection;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -28,6 +29,23 @@ public class ConnectionTest {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(driverManagerDataSource);
     }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10); // 기본 10개
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        // Thread.sleep(1000); //커넥션 풀에서 커넥션 생성 시간 대기
+        // [MyPool connection adder] DEBUG com.zaxxer.hikari.pool.HikariPool - MyPool - Added connection conn2: url=jdbc:h2:tcp://localhost/~/test user=SA
+        // 풀 생성 로그 (10개)
+    }
+
 
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection connection1 = dataSource.getConnection();
