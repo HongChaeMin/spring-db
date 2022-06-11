0543fff6192727acb4dfd43ecd343261ecabb957
Hannah <ghdcoalss33@gmail.com>
2022-06-11 15:19:42 +0900

    feat(transaction): DB lock - select for update

--- sql/lock2.sql
+++ sql/lock2.sql
@@ -7,3 +7,11 @@ update member set money=1000 where member_id = 'memberA';
 commit;
 
 # 결과 적으로는 member.money = 1000
+
+################################################################################
+
+# lock1.sql이 트랜잭션을 종료할 때 까지 대기
+# lock1.sql이 트랜잭션을 종료하면 락을 획득하고 데이터 변경
+# 만약 락 타임아웃이 지나면 락 타임아웃 예외 발생
+set autocommit false;
+update member set money=500 where member_id = 'memberA';
