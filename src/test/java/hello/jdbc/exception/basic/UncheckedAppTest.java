f36c6c3fc0fb79b2f0b5ceeca74d019e667767ba
홍채민 <coals0329@cocen.com>
2022-06-21 18:19:40 +0900

    java(exception): uncheck exception 활용

---
+++ src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -0,0 +1,84 @@
+package hello.jdbc.exception.basic;
+
+import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class UncheckedAppTest {
+
+  @Test
+  void unchecked() {
+    Controller controller = new Controller();
+    Assertions.assertThatThrownBy(controller::request)
+        .isInstanceOf(Exception.class);
+  }
+
+  @Test
+  void printEx() {
+    Controller controller = new Controller();
+    try {
+      controller.request();
+    } catch (Exception e) {
+      log.error("ex", e);
+    }
+  }
+
+  static class Controller {
+    Service service = new Service();
+
+    public void request() { // 컨트롤러와 서비스에서 해당 예외에 대한 의존 관계가 발생하지 않음
+      service.logic();
+    }
+  }
+
+  static class Service {
+    Repository repository = new Repository();
+    NetworkClient networkClient = new NetworkClient();
+
+    public void logic() { // 컨트롤러와 서비스에서 해당 예외에 대한 의존 관계가 발생하지 않음
+      repository.call();
+      networkClient.call();
+    }
+  }
+
+  static class NetworkClient {
+    public void call() {
+      // NetworkClient 는 단순히 기존 체크 예외를 RuntimeConnectException 이라는 런타임 예외가 발생
+      throw new RuntimeConnectException("fail connect");
+    }
+  }
+
+  static class Repository {
+    public void call() {
+      try {
+        runSQL();
+      } catch (SQLException e) {
+        // 체크 예외인 SQLException 이 발생하면 런타임 예외인 RuntimeSQLException 으로 전환해서 예외
+        // 참고로 이때 기존 예외를 포함해주어야 예외 출력시 스택 트레이스에서 기존 예외도 함께 확인
+        throw new RuntimeSQLException(e);
+      }
+    }
+
+    private void runSQL() throws SQLException {
+      throw new SQLException("ex");
+    }
+  }
+
+  // 대부분 복구 불가능한 예외 : 런타임 예외를 사용하면 서비스나 컨트롤러가 이런 복구 불가능한 예외를 신경쓰지 않아도 된다
+  // 의존 관계에 대한 문제 : 런타임 예외는 해당 객체가 처리할 수 없는 예외는 무시
+
+  static class RuntimeConnectException extends RuntimeException {
+    public RuntimeConnectException(String message) {
+      super(message);
+    }
+  }
+
+  static class RuntimeSQLException extends RuntimeException {
+    public RuntimeSQLException(Throwable cause) {
+      super(cause);
+    }
+  }
+
+}
