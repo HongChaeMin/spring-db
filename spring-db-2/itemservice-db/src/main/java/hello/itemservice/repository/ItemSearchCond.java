ffa6060c078aceb83000cd6137c6ac9989fa6985
Hannah <ghdcoalss33@gmail.com>
2022-07-04 10:37:18 +0900

    feat(start): project 구조 설명

--- spring-db-2/itemservice-db/src/main/java/hello/itemservice/repository/ItemSearchCond.java
+++ spring-db-2/itemservice-db/src/main/java/hello/itemservice/repository/ItemSearchCond.java
@@ -3,7 +3,7 @@ package hello.itemservice.repository;
 import lombok.Data;
 
 @Data
-public class ItemSearchCond {
+public class ItemSearchCond { // 검색 조건으로 사용
 
     private String itemName;
     private Integer maxPrice;
