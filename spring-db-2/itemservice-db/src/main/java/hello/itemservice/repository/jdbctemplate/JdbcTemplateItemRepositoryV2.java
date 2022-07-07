9f45d790e89630989b52f09869ada08a78a59e84
Hannah <ghdcoalss33@gmail.com>
2022-07-07 18:14:43 +0900

    feat(jdbc-template): JdbcTemplate - 이름 지정 파라미터 2

--- spring-db-2/itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
+++ spring-db-2/itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -44,6 +44,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
         String sql = "insert into item(item_name, price, quantity)"
             + "values (:itemName, :price, :quantity)";
 
+        // 자바빈 프로퍼티 규약을 통해서 자동으로 파라미터 객체를 생성
         SqlParameterSource param = new BeanPropertySqlParameterSource(item);
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(sql, param, keyHolder);
@@ -59,6 +60,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
             + "set item_name=:itemName, price=:price, quantity=:quantity "
             + "where id=:id";
 
+        // Map 과 유사한데, SQL 타입을 지정할 수 있는 등 SQL에 좀 더 특화된 기능을 제공
         SqlParameterSource param = new MapSqlParameterSource()
             .addValue("itemName", updateParam.getItemName())
             .addValue("price", updateParam.getPrice())
@@ -72,6 +74,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
         String sql = "select id, item_name, price, quantity from item where id=:id";
 
         try {
+            // 단순히 Map을 사용한다
             Map<String, Object> param = Map.of("id", id);
             Item item = jdbcTemplate.queryForObject(sql, new Map[]{param}, itemRowMapper());
             return Optional.of(item);
@@ -85,6 +88,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
         String itemName = cond.getItemName();
         Integer maxPrice = cond.getMaxPrice();
 
+        // 자바빈 프로퍼티 규약을 통해서 자동으로 파라미터 객체를 생성
         SqlParameterSource param = new BeanPropertySqlParameterSource(cond);
 
         String sql = "select id, item_name, price, quantity from item";
@@ -109,6 +113,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
     }
 
     private RowMapper<Item> itemRowMapper() {
+        // ResultSet 의 결과를 받아서 자바빈 규약에 맞추어 데이터를 변환
         return BeanPropertyRowMapper.newInstance(Item.class); //camel 변환 지원
     }
 }
