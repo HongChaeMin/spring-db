d8e006eff928f8534b3b7fcd773299b41e2c6dab
Hannah <ghdcoalss33@gmail.com>
2022-07-08 09:14:01 +0900

    feat(jdbc-template): JdbcTemplate - SimpleJdbcInsert

---
+++ spring-db-2/itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV3.java
@@ -0,0 +1,112 @@
+package hello.itemservice.repository.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+/**
+ * SimpleJdbcInsert
+ **/
+@Slf4j
+@Repository
+public class JdbcTemplateItemRepositoryV3 implements ItemRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
+
+    public JdbcTemplateItemRepositoryV3(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+            .withTableName("item") // 데이터를 저장할 테이블 명을 지정한다
+            .usingGeneratedKeyColumns("id"); // key 를 생성하는 PK 컬럼 명을 지정한다
+        // .usingColumns("item_name", "price", "quantity")  INSERT SQL에 사용할 컬럼을 지정한다. 특정 값만 저장하고 싶을 때 사용한다
+    }
+
+
+    @Override
+    public Item save(Item item) {
+        BeanPropertySqlParameterSource param = new BeanPropertySqlParameterSource(item);
+        Number key = jdbcInsert.executeAndReturnKey(param);
+        item.setId(key.longValue());
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item "
+            + "set item_name=:itemName, price=:price, quantity=:quantity "
+            + "where id=:id";
+
+        // Map 과 유사한데, SQL 타입을 지정할 수 있는 등 SQL에 좀 더 특화된 기능을 제공
+        SqlParameterSource param = new MapSqlParameterSource()
+            .addValue("itemName", updateParam.getItemName())
+            .addValue("price", updateParam.getPrice())
+            .addValue("quantity", updateParam.getQuantity())
+            .addValue("id", itemId); // 이 부분이 별도로 필요하다
+        jdbcTemplate.update(sql, param);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "select id, item_name, price, quantity from item where id=:id";
+
+        try {
+            // 단순히 Map을 사용한다
+            Map<String, Object> param = Map.of("id", id);
+            Item item = jdbcTemplate.queryForObject(sql, new Map[]{param}, itemRowMapper());
+            return Optional.of(item);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        // 자바빈 프로퍼티 규약을 통해서 자동으로 파라미터 객체를 생성
+        SqlParameterSource param = new BeanPropertySqlParameterSource(cond);
+
+        String sql = "select id, item_name, price, quantity from item";
+
+        //동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+        boolean andFlag = false;
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%', :itemName, '%')";
+            andFlag = true;
+        }
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= ?";
+        }
+        log.info("sql={}", sql);
+        return jdbcTemplate.query(sql, new SqlParameterSource[]{param}, itemRowMapper());
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        // ResultSet 의 결과를 받아서 자바빈 규약에 맞추어 데이터를 변환
+        return BeanPropertyRowMapper.newInstance(Item.class); //camel 변환 지원
+    }
+}
